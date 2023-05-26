8ccebc469991e6a635e7e8d920d28e420f5d438c
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-05-26 15:25:11 +0700

    Updated Nav Bottom Bar Links

--- components/NavBottom.tsx
+++ components/NavBottom.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 
 export default function NavBottom() {
@@ -11,28 +11,30 @@ export default function NavBottom() {
 
   return (
     <footer className="fixed bottom-0 z-40 h-12 w-full border-t bg-background">
-      <nav className="grid-cols-auto m-auto grid h-full w-96 grid-flow-col items-baseline  bg-slate-400">
-        <Button variant="">
-          {pathname === "/discover" ? (
-            <Icons.heartSolid className="h-6 w-6 text-blue-500" />
+      <nav className="grid-cols-auto m-auto grid h-full w-96 grid-flow-col items-stretch justify-stretch bg-background align-middle ">
+        <Link href="/" className="flex items-center justify-center ">
+          {pathname === "/" ? (
+            <Icons.homeSolid className="h-6 w-6 " />
+          ) : (
+            <Icons.home className="h-6 w-6" />
+          )}
+        </Link>
+
+        <Link href="/about" className="flex items-center justify-center">
+          {pathname === "/about" ? (
+            <Icons.heartSolid className="h-6 w-6" />
           ) : (
             <Icons.heart className="h-6 w-6" />
           )}
-        </Button>
-        <Button variant="">
-          {pathname === "/discover" ? (
-            <Icons.heartSolid className="h-6 w-6 text-blue-500" />
+        </Link>
+
+        <Link href="/profile" className="flex items-center justify-center">
+          {pathname === "/profile" ? (
+            <Icons.profileSolid className="h-6 w-6 " />
           ) : (
-            <Icons.heart className="h-6 w-6" />
+            <Icons.profile className="h-6 w-6" />
           )}
-        </Button>
-        <Button variant="ghost">
-          {pathname === "/discover" ? (
-            <Icons.heartSolid className="h-6 w-6 text-blue-500" />
-          ) : (
-            <Icons.heart className="h-6 w-6" />
-          )}
-        </Button>
+        </Link>
       </nav>
     </footer>
   )
