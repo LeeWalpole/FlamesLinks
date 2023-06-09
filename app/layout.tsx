f722bd03b2ff2aa98e7c54ee7b113d3d9e422bef
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-06-09 16:01:38 +0700

    Prevent form from zooming in...

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </head>
         <body
           className={cn(
             "min-h-screen bg-background pb-1 font-sans antialiased",
